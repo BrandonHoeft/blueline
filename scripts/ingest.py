53b2bd089ad6ba489c2650bbc8b18cc8e1fb04e6
bhoeft <brandonmhoeft@gmail.com>
2024-07-07 17:51:43 -0500

    1st full-end to end run success to minio S3 bucket

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -2,6 +2,7 @@ import base64
 import json
 import requests
 import datetime
+import dateutil.parser as dparser
 from minio import Minio
 from io import BytesIO
 from prefect import task, Flow
@@ -20,13 +21,25 @@ minio_client = Minio(
     secure=False
 )
 
-def build_url(season, date, endpoint, data_format='json'):
+def build_url(season: str, date: str, endpoint: str, format: str = 'json') -> str:
+    """
+    Parameters
+    ----------
+    season
+        like ''2023-2024-regular' or '2024-playoff'
+    date
+        a valid date in the form of YYYYMMDD
+    endpoint
+        'dfs' has list of slates with games/contests/players, including salaries and fantasy points
+        'dfs_dfs_projections' lists all players who *could* play, along with their projected fantasy points
+    format
+        'json', 'csv', or 'xml'
+    """
     base_url = 'https://api.mysportsfeeds.com/v2.1/pull/nhl'
-    #compiled_url = base_url + '/' + season + '/date/' + date + '/' + endpoint + '.' + format
-    compiled_url = f"{base_url}/{season}/date/{date}/{endpoint}.{data_format}"
+    compiled_url = f"{base_url}/{season}/date/{date}/{endpoint}.{format}"
     return compiled_url
 
-def fetch_data(url, key, pw):
+def fetch_data(url: str, key : str, pw: str) -> Dict[str, Any]:
     # Define the headers with the API key and password encoded in Base64
     headers = {
         "Authorization": "Basic " + base64.b64encode(f'{key}:{pw}'.encode('utf-8')).decode('ascii')
@@ -59,59 +72,51 @@ def fetch_data(url, key, pw):
             print("Ruh Roh! Not authorized. Indicates you're attempting to access a feed for the current in-progress season, without a proper subscription.")
         elif response.status_code == 429:
             print("Ruh Roh! Too many requests. The API limits excessive overuse.")
-        elif response.status_code == 500:
-            print("Ruh Roh! An internal error has occurred, and has been logged for further inspection.")
         elif response.status_code in [499, 502, 503]:
             print("Ruh Roh! Server error. These errors can occur periodically and usually resolve themselves in a matter of seconds.")
         else:
             print(f"Ruh Roh! Unexpected response code: {response.status_code}")
             print(f"Ruh Roh! Response text: {response.text}")
 
-# Example usage
-
-dfs_url = build_url('2023-2024-regular', '20231130', 'dfs')  # list of slates with games/contests/players, including salaries and fantasy points
-dfs_prjctn_url = build_url('2023-2024-regular', '20231130', 'dfs_projections')  # Lists all players who *could* play, along with their projected fantasy points, for each supported DFS source.
-# Example usage
-apikey_token = creds['msf']['key']
-password = creds['msf']['password']
-
-dfs_data = fetch_data(dfs_url, apikey_token, password)
-prjctn_data = fetch_data(dfs_prjctn_url, apikey_token, password)
-
-
 def generate_file_path(provider: str, context: str, dataset_name: str,
-                       schema_version: str) -> str:
-    today = datetime.datetime.today().strftime("%Y-%m-%d")
-    file_path = f"{provider}/{context}/{dataset_name}/v={schema_version}/date={today}/{dataset_name}_{today}.json"
+                       schema_version: str, date: str) -> str:
+    #TODO: Update this
+    # today = datetime.datetime.today().strftime("%Y-%m-%d")
+    file_path = f"{provider}/{context}/{dataset_name}/v={schema_version}/date={date}/{dataset_name}_{date}.json"
     return file_path
+
+def setup_bucket(bucket_name: str) -> None:
+    if not minio_client.bucket_exists(bucket_name):
+        minio_client.make_bucket(bucket_name)
+    # TODO: Update for bucket versioning
+    # minio_client.set_bucket_versioning(bucket_name, "Enabled")
 
 def save_raw_to_minio(data: Dict[str, Any], bucket_name: str,
                       object_name: str) -> None:
+    setup_bucket(bucket_name)
     raw_data = json.dumps(data).encode('utf-8')
     minio_client.put_object(bucket_name, object_name, BytesIO(raw_data),
                             len(raw_data))
 
+if __name__ == "__main__":
+    # Example usage
+    date_str = '20231130'
+    date = dparser.parse(date_str)
+    date = date.strftime('%Y-%m-%d')
+
+    dfs_url = build_url('2023-2024-regular', date_str, 'dfs')
+    dfs_prjctn_url = build_url('2023-2024-regular', date_str, 'dfs_projections')
+    # Example usage
+    apikey_token = creds['msf']['key']
+    password = creds['msf']['password']
+
+    raw_dfs_actuals = fetch_data(dfs_url, apikey_token, password)
+    fp_dfs_actuals = generate_file_path("mysportsfeeds", "dev", "nhl_dfs_actuals", "v2.1", date)
+    save_raw_to_minio(raw_dfs_actuals, "raw-nhl-dfs", fp_dfs_actuals)
+
+    raw_dfs_prjctns = fetch_data(dfs_prjctn_url, apikey_token, password)
+    fp_dfs_prjctns = generate_file_path("mysportsfeeds", "dev", "nhl_prjctn", "v2.1", date)
+    save_raw_to_minio(raw_dfs_prjctns, "raw-nhl-dfs", fp_dfs_prjctns)
 
 # Define the flow
-with Flow("ETL Flow") as flow:
-    api_url_dfs = "https://api.mysportsfeeds.com/v2.1/pull/nhl/2023-2024-regular/date/20231011/dfs.json"
-    api_url_dfs_projections = "https://api.mysportsfeeds.com/v2.1/pull/nhl/2023-2024-regular/date/20231011/dfs_projections.json"
 
-    # Fetch data
-    raw_data_dfs = fetch_data(api_url_dfs)
-    raw_data_dfs_projections = fetch_data(api_url_dfs_projections)
-
-    # Generate file paths
-    file_path_dfs = generate_file_path("mysportsfeeds", "prod", "nhl_dfs",
-                                       "v2.1")
-    file_path_dfs_projections = generate_file_path("mysportsfeeds", "prod",
-                                                   "nhl_dfs_projections",
-                                                   "v2.1")
-
-    # Save raw data to MinIO
-    save_raw_to_minio(raw_data_dfs, "raw-bucket", file_path_dfs)
-    save_raw_to_minio(raw_data_dfs_projections, "raw-bucket",
-                      file_path_dfs_projections)
-
-# Run the flow
-flow.run()
